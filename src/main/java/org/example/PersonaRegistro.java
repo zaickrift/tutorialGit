a5b89f9908d664e8bfeddec08bfe3eeb4588f7cc
zaickrift <danilo.valle.z@gmail.com>
2023-08-16 19:23:17 -0400

    se quito la linea inservible

--- src/main/java/org/example/PersonaRegistro.java
+++ src/main/java/org/example/PersonaRegistro.java
@@ -3,8 +3,7 @@ package org.example;
 public record PersonaRegistro(String nombre, String apellidos) {
     @Override
     public String toString(){
-        //Comente la linea de codigo que consideros se puede eliminar
-        //System.out.println("Esta instruccion es inservible");
+
         return "%s %s".formatted(nombre, apellidos);
     }
 }
