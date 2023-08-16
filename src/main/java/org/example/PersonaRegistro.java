73ec9b6591cef1424124cb34ffc71f45e075077f
zaickrift <48684676+zaickrift@users.noreply.github.com>
2023-08-16 19:18:58 -0400

    comentario sobre codigo inservible

--- src/main/java/org/example/PersonaRegistro.java
+++ src/main/java/org/example/PersonaRegistro.java
@@ -3,7 +3,8 @@ package org.example;
 public record PersonaRegistro(String nombre, String apellidos) {
     @Override
     public String toString(){
-        System.out.println("Esta instruccion es inservible");
+        //Comente la linea de codigo que consideros se puede eliminar
+        //System.out.println("Esta instruccion es inservible");
         return "%s %s".formatted(nombre, apellidos);
     }
 }
