f02c3a0e41ef8ee7c304947d9754534a3e5a92ba
zaickrift <danilo.valle.z@gmail.com>
2023-08-16 22:07:40 -0400

    se reemplaza clase perfona por registro persona

--- src/main/java/org/example/Persona.java
+++ src/main/java/org/example/Persona.java
@@ -1,31 +1,9 @@
 package org.example;
 
-public class Persona {
-    private String nombres,apellidos;
-
-    public Persona(String nombres, String apellidos) {
-        this.nombres = nombres;
-        this.apellidos = apellidos;
-    }
-
-    public String getNombres() {
-        return nombres;
-    }
-
-    public void setNombres(String nombres) {
-        this.nombres = nombres;
-    }
-
-    public String getApellidos() {
-        return apellidos;
-    }
-
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }
-
+public record Persona(String nombre, String apellidos) {
     @Override
-    public String toString() {
-        return "%s %s".formatted(nombres,apellidos);
+    public String toString(){
+
+        return "%s %s".formatted(nombre, apellidos);
     }
 }
--- src/main/java/org/example/PersonaRegistro.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public record PersonaRegistro(String nombre, String apellidos) {
-    @Override
-    public String toString(){
-
-        return "%s %s".formatted(nombre, apellidos);
-    }
-}
